06c100396f9158ec75de57b62c9dccbf03d9fe9c
xepaerz <xepaerz@mail.ru>
2022-01-13 21:56:41 +1000

    УРА ПОБЕДА

--- main.py
+++ main.py
@@ -18,70 +18,54 @@ def main(FileName):
     indexletter = 0
 
     for line in FileREF:
-
         for c in line:
-
             if Censor == True:
-
                 CheckWord = CheckWord + c
-
-                if len(CheckWord) == 3:
-
+                if len(CheckWord) == 2:
                     if not CheckWord.rfind('go') == -1:
-
                         Result = Result + CheckWord.replace('go', '')
                         CheckWord = ''
-
                     else:
-
                         Censor = False
                         Result = Result + '***' + CheckWord
                         CheckWord = ''
-
-                elif indexletter + 3 > countletter:
-
+                elif indexletter + 2 > countletter:
                     if not CheckWord.rfind('go') == -1:
-
                         Result = Result + CheckWord.replace('go', '')
                         CheckWord = ''
-
                     else:
-
                         Censor = False
                         Result = Result + '***' + CheckWord
                         CheckWord = ''
-
             else:
-
-                CheckWord = CheckWord + c
-
-                if len(CheckWord) == 4:
-
-                    if not CheckWord.rfind('ogo') == -1:
-
-                        Censor = True
-                        Result = Result + CheckWord.replace('ogo', '')
-                        CheckWord = ''
-
-                    else:
-
+                if  c == 'o' or c == 'g':
+                    CheckWord = CheckWord + c
+                    if len(CheckWord) == 3:
+                        if not CheckWord.rfind('ogo') == -1:
+                            Censor = True
+                            Result = Result + CheckWord.replace('ogo', '')
+                            CheckWord = ''
+                        else:
+                            Result = Result + CheckWord
+                            CheckWord = ''
+                    elif indexletter + 3 > countletter:
+                        if not CheckWord.rfind('ogo') == -1:
+                            Censor = True
+                            Result = Result + CheckWord.replace('ogo', '')
+                            CheckWord = ''
+                        else:
+                            Result = Result + CheckWord
+                            CheckWord = ''
+                else:
+                    if not CheckWord == '':
                         Result = Result + CheckWord
                         CheckWord = ''
-
-                elif indexletter + 4 > countletter:
-
-                    if not CheckWord.rfind('ogo') == -1:
-
-                        Censor = True
-                        Result = Result + CheckWord.replace('ogo', '')
-                        CheckWord = ''
-
-                    else:
-                        
-                        Result = Result + CheckWord
-                        CheckWord = ''
+                    Result = Result + c
 
             indexletter += 1
+    
+    if Censor == True:
+        Result = Result + '***'
 
     with open ('new_test.txt', 'w') as file:
         file.write(Result)

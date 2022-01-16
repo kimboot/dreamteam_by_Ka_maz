ae3f0cbbf9ac75b1168e7e64b8df29a3f3de44d1
xepaerz <xepaerz@mail.ru>
2022-01-16 23:09:24 +1000

    Начало 2 алгоритма

---
+++ Task_2/main.py
@@ -0,0 +1,6 @@
+# License dream team by ka maz
+# project by xepaerZ & kulinarrr
+
+Number_TShirts = [0,1,0,1,1,0] # кол-во футболок (от S до XXXL, цифры перечислены в порядке возрастания размеров)
+Number_Participants = 3
+
